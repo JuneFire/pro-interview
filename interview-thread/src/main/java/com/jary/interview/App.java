586c72cc43f4d49e8202d1e75231a78f5429c567
程 <chengzhikang2015@163.com>
2018-07-22 16:29:14 +0800

    第一个面试题

---
+++ interview-thread/src/main/java/com/jary/interview/App.java
@@ -0,0 +1,13 @@
+package com.jary.interview;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
