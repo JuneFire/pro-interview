fa41fc9ee22fb4f258fcaff55c6a7097ec70fb9d
橙汁 <chengzhikang2015@163.com>
2020-07-15 23:07:55 +0800

    1. 两数之和
    给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
    你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
    
    示例:
    给定 nums = [2, 7, 11, 15], target = 9
    因为 nums[0] + nums[1] = 2 + 7 = 9
    所以返回 [0, 1]

---
+++ pro-data-structure/src/main/java/com/jary/interview/leetcode/Solution.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * @Author 刘建广
+ * @Date 2020/7/15 22:15
+ * @Version 1.0
+ */
+public class Solution {
+
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] a = new int[2];
+
+        HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int b = target - nums[i];
+            if(hashMap.containsKey(b)){
+                a[0] = hashMap.get(b);
+                a[1] = i;
+                break;
+            }else {
+                hashMap.put(nums[i],i);
+            }
+        }
+
+        return a;
+    }
+
+    public static void main(String[] args){
+        int[] nums = new int[]{15, 11, 2, 7};
+        int target = 9;
+        Solution solution = new Solution();
+        int[] ints =  solution.twoSum(nums, target);
+        System.out.println("答案："+ Arrays.toString(ints));
+    }
+}
