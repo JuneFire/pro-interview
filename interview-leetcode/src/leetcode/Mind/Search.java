4a00623cffa45c19f3755d4f91d670cf0fc03086
zkcheng <chengzhikang2015@163.com>
2021-06-24 21:59:27 +0800

    "Leetcode 题解 - 搜索算法"

---
+++ interview-leetcode/src/leetcode/Mind/Search.java
@@ -0,0 +1,284 @@
+package leetcode.Mind;
+
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @Author: zkcheng
+ * @Date: 2021/06/24/21:52
+ * @Description:
+ */
+public class Search {
+
+    /**
+     * 1. 计算在网格中从原点到特定点的最短路径长度
+     * 1091. Shortest Path in Binary Matrix(Medium)
+     *
+     * BFS查找最短路径
+     */
+    public int shortestPathBinaryMatrix(int[][] grids) {
+        if (grids == null || grids.length == 0 || grids[0].length == 0) {
+            return -1;
+        }
+        int[][] direction = {{1, -1}, {1, 0}, {1, 1}, {0, -1}, {0, 1}, {-1, -1}, {-1, 0}, {-1, 1}};
+        int m = grids.length, n = grids[0].length;
+        Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
+        queue.add(new Pair<>(0, 0));
+        int pathLength = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            pathLength++;
+            while (size-- > 0) {
+                Pair<Integer, Integer> cur = queue.poll();
+                int cr = cur.getKey(), cc = cur.getValue();
+                if (grids[cr][cc] == 1) {
+                    continue;
+                }
+                if (cr == m - 1 && cc == n - 1) {
+                    return pathLength;
+                }
+                grids[cr][cc] = 1; // 标记
+                for (int[] d : direction) {
+                    int nr = cr + d[0], nc = cc + d[1];
+                    if (nr < 0 || nr >= m || nc < 0 || nc >= n) {
+                        continue;
+                    }
+                    queue.add(new Pair<>(nr, nc));
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 279. 完全平方数
+     * 给定正整数 n，找到若干个完全平方数（比如 1, 4, 9, 16, ...）使得它们的和等于 n。你需要让组成和的完全平方数的个数最少。
+     * todo  这种方法还是不太明白
+     */
+    public int numSquares(int n) {
+        List<Integer> squares = generateSquares(n);
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] marked = new boolean[n + 1];
+        queue.add(n);
+        marked[n] = true;
+        int level = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            level++;
+            while (size-- > 0) {
+                int cur = queue.poll();
+                for (int s : squares) {
+                    int next = cur - s;
+                    if (next < 0) {
+                        break;
+                    }
+                    if (next == 0) {
+                        return level;
+                    }
+                    if (marked[next]) {
+                        continue;
+                    }
+                    marked[next] = true;
+                    queue.add(next);
+                }
+            }
+        }
+        return n;
+    }
+
+    /**
+     * 生成小于 n 的平方数序列
+     * @return 1,4,9,...
+     */
+    private List<Integer> generateSquares(int n) {
+        List<Integer> squares = new ArrayList<>();
+        int square = 1;
+        int diff = 3;
+        while (square <= n) {
+            squares.add(square);
+            square += diff;
+            diff += 2;
+        }
+        return squares;
+    }
+
+
+    /**
+     * 3. 最短单词路径
+     * 127. Word Ladder (Medium)
+     *
+     * BFS
+     */
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        wordList.add(beginWord);
+        int N = wordList.size();
+        int start = N - 1;
+        int end = 0;
+        while (end < N && !wordList.get(end).equals(endWord)) {
+            end++;
+        }
+        if (end == N) {
+            return 0;
+        }
+        List<Integer>[] graphic = buildGraphic(wordList);
+        return getShortestPath(graphic, start, end);
+    }
+
+    /**
+     * 建图
+     * @param wordList
+     * @return
+     */
+    private List<Integer>[] buildGraphic(List<String> wordList) {
+        int N = wordList.size();
+        List<Integer>[] graphic = new List[N];
+        for (int i = 0; i < N; i++) {
+            graphic[i] = new ArrayList<>();
+            for (int j = 0; j < N; j++) {
+                if (isConnect(wordList.get(i), wordList.get(j))) {
+                    graphic[i].add(j);
+                }
+            }
+        }
+        return graphic;
+    }
+
+    /**
+     * 判断s1 和 s2是否可通
+     * @param s1
+     * @param s2
+     * @return
+     */
+    private boolean isConnect(String s1, String s2) {
+        int diffCnt = 0;
+        for (int i = 0; i < s1.length() && diffCnt <= 1; i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                diffCnt++;
+            }
+        }
+        return diffCnt == 1;
+    }
+
+    /**
+     * 生成最短路径长度
+     * @param graphic
+     * @param start
+     * @param end
+     * @return
+     */
+    private int getShortestPath(List<Integer>[] graphic, int start, int end) {
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] marked = new boolean[graphic.length];
+        queue.add(start);
+        marked[start] = true;
+        int path = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            path++;
+            while (size-- > 0) {
+                int cur = queue.poll();
+                for (int next : graphic[cur]) {
+                    if (next == end) {
+                        return path;
+                    }
+                    if (marked[next]) {
+                        continue;
+                    }
+                    marked[next] = true;
+                    queue.add(next);
+                }
+            }
+        }
+        return 0;
+    }
+
+
+    /**
+     * DFS
+     * 1. 查找最大的连通面积
+     * 695. Max Area of Island (Medium)
+     * [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+     *  [0,0,0,0,0,0,0,1,1,1,0,0,0],
+     *  [0,1,1,0,1,0,0,0,0,0,0,0,0],
+     *  [0,1,0,0,1,1,0,0,1,0,1,0,0],
+     *  [0,1,0,0,1,1,0,0,1,1,1,0,0],
+     *  [0,0,0,0,0,0,0,0,0,0,1,0,0],
+     *  [0,0,0,0,0,0,0,1,1,1,0,0,0],
+     *  [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+     */
+    private int m, n;
+    private int[][] direction = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    public int maxAreaOfIsland(int[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        m = grid.length;
+        n = grid[0].length;
+        int maxArea = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                maxArea = Math.max(maxArea, dfs(grid, i, j));
+            }
+        }
+        return maxArea;
+    }
+
+    private int dfs(int[][] grid, int r, int c) {
+        if (r < 0 || r >= m || c < 0 || c >= n || grid[r][c] == 0) {
+            return 0;
+        }
+        grid[r][c] = 0;
+        int area = 1;
+        for (int[] d : direction) {
+            area += dfs(grid, r + d[0], c + d[1]);
+        }
+        return area;
+    }
+
+    /**
+     * 2. 矩阵中的连通分量数目
+     * 200. Number of Islands (Medium)
+     * 可以将矩阵表示看成一张有向图。
+     * Input:
+     * 11000
+     * 11000
+     * 00100
+     * 00011
+     *
+     * Output: 3
+     */
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        m = grid.length;
+        n = grid[0].length;
+        int islandsNum = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] != '0') {
+                    dfs(grid, i, j);
+                    islandsNum++;
+                }
+            }
+        }
+        return islandsNum;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        for (int[] d : direction) {
+            dfs(grid, i + d[0], j + d[1]);
+        }
+    }
+
+}
