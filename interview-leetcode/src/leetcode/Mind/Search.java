57fbee4cdbcdafaf95997477368a25f7dc72f9bb
zkcheng <chengzhikang2015@163.com>
2021-06-25 22:37:09 +0800

    "Leetcode 题解 - 搜索算法"

--- interview-leetcode/src/leetcode/Mind/Search.java
+++ interview-leetcode/src/leetcode/Mind/Search.java
@@ -2,10 +2,8 @@ package leetcode.Mind;
 
 import javafx.util.Pair;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
 
 /**
  * @Author: zkcheng
@@ -279,6 +277,171 @@ public class Search {
         for (int[] d : direction) {
             dfs(grid, i + d[0], j + d[1]);
         }
+    }
+
+    /**
+     * 4. 填充封闭区域
+     * 130. Surrounded Regions (Medium)
+     *
+     * 题目描述：使被 'X' 包围的 'O' 转换为 'X'。
+     * 先填充最外侧，剩下的就是里侧了。
+     * @param board
+     */
+
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0) {
+            return;
+        }
+
+        m = board.length;
+        n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            dfs2(board, i, 0);
+            dfs2(board, i, n - 1);
+        }
+        for (int i = 0; i < n; i++) {
+            dfs2(board, 0, i);
+            dfs2(board, m - 1, i);
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'T') {
+                    board[i][j] = 'O';
+                } else if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+
+    private void dfs2(char[][] board, int r, int c) {
+        if (r < 0 || r >= m || c < 0 || c >= n || board[r][c] != 'O') {
+            return;
+        }
+        board[r][c] = 'T';
+        for (int[] d : direction) {
+            dfs(board, r + d[0], c + d[1]);
+        }
+    }
+
+    /**
+     * 5. 能到达的太平洋和大西洋的区域
+     * 417. Pacific Atlantic Water Flow (Medium)
+     * Given the following 5x5 matrix:
+     *
+     *   Pacific ~   ~   ~   ~   ~
+     *        ~  1   2   2   3  (5) *
+     *        ~  3   2   3  (4) (4) *
+     *        ~  2   4  (5)  3   1  *
+     *        ~ (6) (7)  1   4   5  *
+     *        ~ (5)  1   1   2   4  *
+     *           *   *   *   *   * Atlantic
+     *
+     * Return:
+     * [[0, 4], [1, 3], [1, 4], [2, 2], [3, 0], [3, 1], [4, 0]] (positions with parentheses in above matrix).
+     * 左边和上边是太平洋，右边和下边是大西洋，内部的数字代表海拔，海拔高的地方的水能够流到低的地方，求解水能够流到太平洋和大西洋的所有位置。
+     */
+    private int[][] matrix;
+
+    public List<List<Integer>> pacificAtlantic(int[][] matrix) {
+        List<List<Integer>> ret = new ArrayList<>();
+        if (matrix == null || matrix.length == 0) {
+            return ret;
+        }
+
+        m = matrix.length;
+        n = matrix[0].length;
+        this.matrix = matrix;
+        boolean[][] canReachP = new boolean[m][n];
+        boolean[][] canReachA = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            dfs(i, 0, canReachP);
+            dfs(i, n - 1, canReachA);
+        }
+        for (int i = 0; i < n; i++) {
+            dfs(0, i, canReachP);
+            dfs(m - 1, i, canReachA);
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (canReachP[i][j] && canReachA[i][j]) {
+                    ret.add(Arrays.asList(i, j));
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    private void dfs(int r, int c, boolean[][] canReach) {
+        if (canReach[r][c]) {
+            return;
+        }
+        canReach[r][c] = true;
+        for (int[] d : direction) {
+            int nextR = d[0] + r;
+            int nextC = d[1] + c;
+            if (nextR < 0 || nextR >= m || nextC < 0 || nextC >= n
+                    || matrix[r][c] > matrix[nextR][nextC]) {
+
+                continue;
+            }
+            dfs(nextR, nextC, canReach);
+        }
+    }
+
+    /**
+     * Backtracking
+     *
+     * Backtracking（回溯）属于 DFS。
+     *
+     * 普通 DFS 主要用在 可达性问题 ，这种问题只需要执行到特点的位置然后返回即可。
+     * 而 Backtracking 主要用于求解 排列组合 问题，例如有 { 'a','b','c' } 三个字符，求解所有由这三个字符排列得到的字符串，
+     * 这种问题在执行到特定的位置返回之后还会继续执行求解过程。
+     * 因为 Backtracking 不是立即返回，而要继续求解，因此在程序实现时，需要注意对元素的标记问题：
+     *
+     * 在访问一个新元素进入新的递归调用时，需要将新元素标记为已经访问，这样才能在继续递归调用时不用重复访问该元素；
+     * 但是在递归返回时，需要将元素标记为未访问，因为只需要保证在一个递归链中不同时访问一个元素，可以访问已经访问过但是不在当前递归链中的元素。
+     */
+
+    /**
+     * 1. 数字键盘组合
+     * 17. Letter Combinations of a Phone Number (Medium)
+     */
+    private static final String[] KEYS = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    public List<String> letterCombinations(String digit){
+        List<String> combination = new ArrayList<>();
+        if(digit == null || digit.length() == 0){
+            return combination;
+        }
+        doCombination(new StringBuilder(), combination, digit) ;
+        return combination;
+    }
+
+    public void doCombination(StringBuilder prefix, List<String> combination, final String digit){
+        if(prefix.length() == digit.length()){  //达到标准就退出
+            combination.add(prefix.toString());
+            return;
+        }
+        int curDigit = digit.charAt(prefix.length()) - '0'; // 获取匹配的字母
+        String letters = KEYS[curDigit];
+        for(char c : letters.toCharArray()){
+            prefix.append(c);  //添加
+            doCombination(prefix, combination, digit);
+            prefix.deleteCharAt(prefix.length() - 1); //删除
+        }
+    }
+
+
+
+    public static void main(String[] args){
+        Search search = new Search();
+        System.out.println(search.letterCombinations("234"));
     }
 
 }
